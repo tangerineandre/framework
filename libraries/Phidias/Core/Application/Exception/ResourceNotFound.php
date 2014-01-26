9a756d9f4184a487aeeb2f0dc21528837c264db5
Santiago Cortes <santiago.cortes@phidias.com.co>
2014-01-26 14:33:58 -0500

    removes unused classes

--- libraries/Phidias/Core/Application/Exception/ResourceNotFound.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Phidias\Core\Application\Exception;
-
-class ResourceNotFound extends \Phidias\Exception
-{
-	protected $code = 404;
-}
--- libraries/Phidias/Core/Application/Exception/WrongArgumentCount.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Phidias\Core\Application\Exception;
-
-class WrongArgumentCount extends \Phidias\Exception
-{
-	protected $code = 404;
-}
