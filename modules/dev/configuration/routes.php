f3acd77dd4e53df19855d100c35da093b6163e38
Santiago Cortes <santiago.cortes@phidias.com.co>
2014-03-05 14:24:45 -0500

    added missing routes for database admin resources

--- modules/dev/configuration/routes.php
+++ modules/dev/configuration/routes.php
@@ -2,3 +2,8 @@
 use Phidias\Resource\Route;
 
 Route::forRequest("GET phidias/db/create")->useController(array('Phidias\DB\Controller', 'getCreate'));
+Route::forRequest("GET phidias/db/drop")->useController(array('Phidias\DB\Controller', 'getDrop'));
+Route::forRequest("GET phidias/db/recreate")->useController(array('Phidias\DB\Controller', 'getRecreate'));
+Route::forRequest("GET phidias/db/triggers")->useController(array('Phidias\DB\Controller', 'getTriggers'));
+Route::forRequest("GET phidias/db/truncate")->useController(array('Phidias\DB\Controller', 'getTruncate'));
+Route::forRequest("GET phidias/db/optimize")->useController(array('Phidias\DB\Controller', 'getOptimize'));
